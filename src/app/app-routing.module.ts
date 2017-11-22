417b23669aa2e946e3ae6689242b624f4632d3b4
gongxufan <gongxufan@126.com>
2017-11-22 17:21:25 +0800

    test fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HeroesComponent} from "./component/heroes/heroes.component";
-import {DashboardComponent} from "./component/dashboard/dashboard.component";
-import {HeroDetailComponent} from "./component/hero-detail/hero-detail.component";
+import {HeroesComponent} from './component/heroes/heroes.component';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
+import {HeroDetailComponent} from './component/hero-detail/hero-detail.component';
 
 const routes: Routes = [
   //默认路由
